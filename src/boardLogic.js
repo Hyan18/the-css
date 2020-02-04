d838672b19ad0671ee63e2cef4ff64e3142230b9
Jamie95187 <jamiewong63@googlemail.com>
2020-02-04 18:47:55 +0000

    refactor: isolate boardLogic (still has failing feature tests)

--- src/boardLogic.js
+++ src/boardLogic.js
@@ -1,32 +1,32 @@
 export default class BoardLogic {
 
-  constructor(initialGrid) {
-    this.grid = initialGrid;
-  };
+  constructor(initialGrid, Cell) {
+    this.grid = initialGrid.map(row => row.map(state => new Cell(state)))
+    this.grid.forEach((row, i) => row.forEach((cell, j) => {
+      const xLowerBound = (i > 0 ? i - 1 : i)
+      const xUpperBound = (i < this.grid.length-1 ? i + 1 : i)
 
-  iterate() {
-    this.grid = this.grid.map((row, i) => row.map((cell, j) => {
-      const xLowerBound = (i > 0 ? i - 1 : i);
-      const xUpperBound = (i < this.grid.length-1 ? i + 1 : i);
+      const yLowerBound = (j > 0 ? j - 1 : j)
+      const yUpperBound = (j < this.grid.length-1 ? j + 1 : j)
 
-      const yLowerBound = (j > 0 ? j - 1 : j);
-      const yUpperBound = (j < this.grid.length-1 ? j + 1 : j);
-
-      let sum = 0;
+      let neighbours = []
       for (let x = xLowerBound; x <= xUpperBound; x++) {
         for (let y = yLowerBound; y <= yUpperBound; y++) {
           if (x !== i || y !== j) {
-            sum += this.grid[x][y];
+            neighbours.push(this.grid[x][y])
           }
         }
       }
-      if (sum === 3) return 1;
-      if (sum === 2 && cell === 1) return 1;
-      return 0;
-    }));
-  };
+      cell.setNeighbours(neighbours)
+    }))
+  }
+
+  iterate() {
+    this.grid.forEach(row => row.forEach(cell => cell.setNextState()))
+    this.grid.forEach(row => row.forEach(cell => cell.updateState()))
+  }
 
   cellStates() {
-    return this.grid;
-  };
+    return this.grid.map(row => row.map(cell => cell.currentState()))
+  }
 }
--- src/boardLogic.test.js
+++ src/boardLogic.test.js
@@ -1,50 +1,82 @@
-import BoardLogic from './boardLogic';
+import BoardLogic from './boardLogic'
 
-describe('BoardLogic test', () => {
+describe('BoardLogic', () => {
 
-  testBoard('empty 1x1 grid', [[0]], [[0]]);
+  const myCell = { setNeighbours: jest.fn(),
+                   setNextState: jest.fn(),
+                   updateState: jest.fn(),
+                   currentState: jest.fn(() => 0) }
+  const myCellConstructor = jest.fn(x => myCell)
 
-  testBoard('alive one by one grid', [[1]], [[0]]);
+  afterEach(() => {
+    myCell.currentState.mockClear()
+    myCell.setNeighbours.mockClear()
+    myCell.setNextState.mockClear()
+    myCell.updateState.mockClear()
+    myCellConstructor.mockClear()
+  })
 
-  testBoard('empty 2x2 grid', [[0,0],[0,0]], [[0,0],[0,0]]);
+  describe('new BoardLogic', () => {
 
-  testBoard('one alive cell in a 2x2', [[1,0],[0,0]], [[0,0],[0,0]]);
+    test('should convert initial states to cells', () => {
+      const boardLogic = new BoardLogic([[0]], myCellConstructor)
+      expect(myCellConstructor.mock.calls.length).toBe(1)
+      expect(myCellConstructor.mock.calls[0][0]).toBe(0)
 
-  testBoard('4 alive cells in a 2x2', [[1,1],[1,1]], [[1,1],[1,1]]);
+      myCellConstructor.mockClear()
+      const boardLogic2 = new BoardLogic([[1]], myCellConstructor)
+      expect(myCellConstructor.mock.calls[0][0]).toBe(1)
 
-  testBoard('3 alive cells in a 2x2', [[1,1],[1,0]], [[1,1],[1,1]]);
+      myCellConstructor.mockClear()
+      const boardLogic3 = new BoardLogic([[0,0],[1,1]], myCellConstructor)
+      expect(myCellConstructor.mock.instances.length).toBe(4)
 
-  testBoard('2 alive cells in a 2x2', [[1,1],[0,0]], [[0,0],[0,0]]);
+    })
 
-  testBoard('empty 3x3', [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]);
+    test('should set neighbours', () => {
+      const boardLogic = new BoardLogic([[0]], myCellConstructor)
+      expect(myCell.setNeighbours.mock.calls.length).toBe(1)
+    })
 
-  testBoard('overcrowding', [[1,1,1],[1,1,0],[0,0,0]], [[1,0,1],[1,0,1],[0,0,0]]);
+    test('should set neighbours 4 times', () => {
+      const boardLogic = new BoardLogic([[1,0],[1,0]], myCellConstructor)
+      expect(myCell.setNeighbours.mock.calls.length).toBe(4)
+      myCell.setNeighbours.mock.calls.forEach(args => {
+        expect(args[0].length).toBe(3)
+      })
+    })
 
-  testBoard('9 alive cells in 3x3', [[1,1,1],[1,1,1],[1,1,1]], [[1,0,1],[0,0,0],[1,0,1]]);
+  })
 
-  testBoard('isolating overcrowding',
-    [
-      [0,0,1,0,0],
-      [0,1,0,1,0],
-      [1,0,1,0,1],
-      [0,1,0,1,0],
-      [0,0,1,0,0]
-    ],
-    [
-      [0,0,1,0,0],
-      [0,1,0,1,0],
-      [1,0,0,0,1],
-      [0,1,0,1,0],
-      [0,0,1,0,0]
-    ]
-  );
+  describe('.iterate', () => {
 
-  function testBoard(description, initialGrid, expectedGrid) {
-    test(description, () => {
-      const board = new BoardLogic(initialGrid);
-      board.iterate();
-      expect(board.cellStates()).toEqual(expectedGrid);
-    });
-  };
+    test('should call setNextState for each cell', () => {
+      const boardLogic = new BoardLogic([[1,0],[1,0]], myCellConstructor)
+      boardLogic.iterate()
+      expect(myCell.setNextState.mock.calls.length).toBe(4)
+    })
 
-});
+    test('should call updateState for each cell', () => {
+      const boardLogic = new BoardLogic([[1,0],[1,0]], myCellConstructor)
+      boardLogic.iterate()
+      expect(myCell.updateState.mock.calls.length).toBe(4)
+    })
+
+  })
+
+  describe('.cellStates', () => {
+
+    test('call current state on each cell', () => {
+      const boardLogic = new BoardLogic([[1,0],[1,0]], myCellConstructor)
+      boardLogic.cellStates();
+      expect(myCell.currentState.mock.calls.length).toBe(4)
+    })
+
+    test('should return current states in a 2D array', () => {
+      const boardLogic = new BoardLogic([[1,0],[1,0]], myCellConstructor)
+      expect(boardLogic.cellStates()).toEqual([[0,0],[0,0]])
+    })
+
+  })
+
+})
