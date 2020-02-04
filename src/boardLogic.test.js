921af9a8936c04e1c69b06bf5fc9f369c5a37c84
Jamie95187 <jamiewong63@googlemail.com>
2020-02-04 14:37:37 +0000

    test: add tests for overcrowding

--- src/boardLogic.test.js
+++ src/boardLogic.test.js
@@ -16,6 +16,29 @@ describe('BoardLogic test', () => {
 
   testBoard('2 alive cells in a 2x2', [[1,1],[0,0]], [[0,0],[0,0]]);
 
+  testBoard('empty 3x3', [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]);
+
+  testBoard('overcrowding', [[1,1,1],[1,1,0],[0,0,0]], [[1,0,1],[1,0,1],[0,0,0]]);
+
+  testBoard('9 alive cells in 3x3', [[1,1,1],[1,1,1],[1,1,1]], [[1,0,1],[0,0,0],[1,0,1]]);
+
+  testBoard('isolating overcrowding',
+    [
+      [0,0,1,0,0],
+      [0,1,0,1,0],
+      [1,0,1,0,1],
+      [0,1,0,1,0],
+      [0,0,1,0,0]
+    ],
+    [
+      [0,0,1,0,0],
+      [0,1,0,1,0],
+      [1,0,0,0,1],
+      [0,1,0,1,0],
+      [0,0,1,0,0]
+    ]
+  );
+
   function testBoard(description, initialGrid, expectedGrid) {
     test(description, () => {
       const board = new BoardLogic(initialGrid);
