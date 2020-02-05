55c72c74b23377c9bf91a170db1fe053d26dbf13
Hyan18 <Hyan18@github.com>
2020-02-05 17:11:12 +0000

    feat: toggle state from board

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import './Board.css';
+import Cell from '../Cell/Cell'
 
+const CELL_SIZE = 60
+const WIDTH = 600
+const HEIGHT = 600
 const ROWS = 10
 const COLS = 10
 
@@ -20,21 +24,27 @@ class Board extends Component {
           board[y][x] = 0;
       }
     }
-
     return board;
+  }
+
+  handleClick(x, y, state) {
+    let cells = this.state.cells.slice()
+
+    cells[y][x] = (state + 1) % 2
+
+    this.setState( { cells: cells } )
   }
 
   render = () => {
     let currentState = this.state.cells
 
     return (
-      <div className="board-div">
+      <div className="board-div" style={{ width: WIDTH, height: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}>
         {currentState.map((row, i) => row.map((cell, j) =>
-          (<div className="Cell0"/>)
+          (<Cell x={j} y={i} state={cell} key={`${j}, ${i}`} onClick={ () => this.handleClick(j, i, cell) }/>)
         ))}
       </div>
     )
   }
 }
-
 export default Board
--- src/components/Board/Board.spec.js
+++ src/components/Board/Board.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { shallow, mount } from 'enzyme'
+import { shallow, mount, render } from 'enzyme'
 import Board from './Board'
 
 describe('Board', () => {
@@ -11,9 +11,21 @@ describe('Board', () => {
     expect(wrapper.find('.board-div').length).toEqual(1)
   });
 
-  it('should render a 10x10 empty grid', () => {
+  it('should render a 10x10 grid', () => {
+    const wrapper = shallow(<Board />)
+    const total = 10 * 10
+
+    expect(wrapper.children('Cell').length).toEqual(total)
+  })
+
+  it('should turn a cell to alive', () => {
     const wrapper = shallow(<Board />)
 
-    expect(wrapper.children('.Cell0').length).toEqual(100)
+    let cell = wrapper.children().find('Cell').findWhere(n => n.prop('x') === 0 && n.prop('y') === 0)
+
+    cell.simulate('click')
+    cell = wrapper.children().find('Cell').findWhere(n => n.prop('x') === 0 && n.prop('y') === 0)
+    expect(cell.prop('state')).toBe(1)
   })
+
 })
