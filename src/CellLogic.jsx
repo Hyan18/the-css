f63332642bb6c3742020d301d3d78cfc424fe0d1
Student <student@Makerss-MacBook-Air.local>
2020-02-05 11:33:43 +0000

    test: add Neighbour function

--- src/CellLogic.jsx
+++ src/CellLogic.jsx
@@ -1,9 +1,16 @@
 export default class CellLogic {
   constructor (initialState) {
     this.state = initialState
+    this.neighbours = []
   }
 
   currentState () {
     return this.state
   }
+
+  addNeighbour (neighbour) {
+    this.neighbours.push(neighbour)
+    return this.neighbours
+  }
+
 }
--- src/CellLogic.spec.js
+++ src/CellLogic.spec.js
@@ -2,7 +2,23 @@ import CellLogic from './CellLogic'
 
 describe('CellLogic', () => {
   it('can return current state when initialized with integer', () => {
-    const cell = new CellLogic(5)
-    expect(cell.currentState()).toEqual(5)
+    const cell = new CellLogic(0)
+    expect(cell.currentState()).toEqual(0)
+  })
+
+  describe("Add neighbour", () => {
+    it("Should return an array of the first neighbour", () => {
+      const cell = new CellLogic(1)
+      const neighbour = new CellLogic(0)
+      expect(cell.addNeighbour(neighbour)).toEqual([neighbour])
+    })
+
+    it("Should return an array of two neighbours", () => {
+      const cell = new CellLogic(1)
+      const neighbour1 = new CellLogic(1)
+      const neighbour2 = new CellLogic(0)
+      cell.addNeighbour(neighbour1)
+      expect(cell.addNeighbour(neighbour2)).toEqual([neighbour1, neighbour2])
+    })
   })
 })
