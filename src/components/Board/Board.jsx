aa0ee2acf8fadbc7549ceb6cb8ef3e17d2a988e6
Jamie95187 <jamiewong63@googlemail.com>
2020-02-05 11:40:14 +0000

    refactor: board logic test

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -3,25 +3,27 @@ import './Board.css';
 import BoardLogic from '../BoardLogic/BoardLogic'
 import Cell from '../Cell/Cell'
 
-const CELL_SIZE = 200;
+const CELL_SIZE = 60;
 const WIDTH = 600;
 const HEIGHT = 600;
 
 class Board extends Component {
   constructor () {
     super()
-    this.boardLogic = new BoardLogic ([[0,1,0],[0,1,0],[0,1,0]])
+    // this.state = {
+    //
+    // }
   }
-  
+
   render = () => {
-    const currentState = this.boardLogic.cellStates()
+    // const currentState = this.boardLogic.cellStates()
 
+    // {currentState.map((row, i) => row.map((cell, j) =>
+    // (<Cell x={j} y={i} state={cell}/>)
+    // ))}
     return (
-
       <div className="board-div" style={{ width: WIDTH, height: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px` }}>
-        {currentState.map((row, i) => row.map((cell, j) => 
-        (<Cell x={j} y={i} state={cell}/>)
-        ))}
+
       </div>
     )
   }
--- src/components/Board/Board.spec.js
+++ src/components/Board/Board.spec.js
@@ -3,11 +3,22 @@ import { shallow } from 'enzyme'
 import Board from './Board'
 
 describe('Board', () => {
-  let wrapper
-  beforeEach(() => (wrapper = shallow(<Board />)))
+
+  const wrapper = shallow(<Board />)
 
   it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+    console.log(wrapper.find('.board-div'))
+    expect(wrapper.find('.board-div')).toBeDefined()
+    expect(wrapper.find('.board-div').length).toEqual(1)
   });
 
+  // it('should render a 10x10 empty grid', () => {
+  //   const wrapper = shallow((
+  //     <Board>
+  //       <div className="board-div">
+  //         <div className='Cell0'></div>
+  //       </div>
+  //     </Board>))
+  //   expect(wrapper.contains(<div className='Cell0'></div>)).toBeTruthy()
+  // })
 })
