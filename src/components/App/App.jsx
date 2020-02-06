6536fd191ac52f74c476637e8d81dbbc60c70223
Dawid Szpener <dawidszpener141@gmail.com>
2020-02-06 14:22:11 +0000

    feat: added description to GoL

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Board from '../Board/Board'
+import './App.css'
+import { Text } from '../Text/Text'
 
 const App = () => (
+  
   <div className="app-container">
+    <div className="description-div">
+      <Text />
+    </div>
     <Board />
   </div>
 );
