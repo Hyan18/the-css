589b7ab352f43abffc5b30eb5a3619311ac0dfa0
Dawid Szpener <dawidszpener141@gmail.com>
2020-02-07 12:13:50 +0000

    feat: board size can be customized by user

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -11,12 +11,15 @@ const COLS = 60
 
 class Board extends Component {
   constructor () {
-    super()
+    super() 
     this.state = {
       cells: this.emptyBoard(),
       rows: 60,
       cols: 60
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.changeBoardSize = this.changeBoardSize.bind(this);
   }
 
   emptyBoard() {
@@ -24,7 +27,18 @@ class Board extends Component {
     for (let y = 0; y < ROWS; y++) {
       board[y] = [];
       for (let x = 0; x < COLS; x++) {
-          board[y][x] = 0;
+        board[y][x] = 0;
+      }
+    }
+    return board
+  }
+
+  boardRerender() {
+    let board = []
+    for (let y = 0; y < this.state.rows; y++) {
+      board[y] = [];
+      for (let x = 0; x < this.state.cols; x++) {
+        board[y][x] = 0;
       }
     }
     return board
@@ -44,27 +58,42 @@ class Board extends Component {
     this.setState( { cells: cells } )
   }
 
-  changeBoardSize = (event) => {
-    this.setState({ rows: 2})
-    this.setState({ cols: 3})
+  handleChange(event) {
+    if(event.target.value <= 60 )
+    if(event.target.name === 'rows')
+        this.setState({rows: parseInt(event.target.value, 10)})
+    else
+        this.setState({cols: parseInt(event.target.value, 10)})
+  }
+
+  changeBoardSize() {
+    this.setState( {
+      rows: this.state.rows,
+      cols: this.state.cols,
+      cells: this.boardRerender()
+    })
   }
 
   render = () => {
     let currentState = this.state.cells
 
     return (
-      <div class="board-container">
+      <div className="board-container">
         <div className="board-div" style={{ width: WIDTH, maxWidth: WIDTH, height: HEIGHT, maxHeight: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}>
           {currentState.map((row, i) => row.map((cell, j) =>
             (<Cell x={j} y={i} state={cell} key={`${j}, ${i}`} onClick={ () => this.handleClick(j, i, cell) }/>)
           ))}
         </div>
         <div className="controls">
-          <button className="button" onClick={this.iterate}>Iterate</button>  
+          <button className="button" onClick={this.iterate}>Iterate</button>
           <form>
-              <input type="text" ref="rows" name="rows" />
-              <input type="text" ref="cols" name="cols" />
-              <button type="button" onClick={this.changeBoardSize}>Update</button>
+            <label>
+              Rows:
+              <input type="number" name="rows" placeholder="max 60" onChange={this.handleChange}/>
+              Cols:
+              <input type="number" name="cols" placeholder="max 60" onChange={this.handleChange}/> 
+            </label>
+            <button type="button" onClick={this.changeBoardSize}>Update</button>
             </form>
         </div>
       </div>
