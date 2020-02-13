521f70f89f57f13119353c22c4a5ab4b4bd8eb2a
Dawid Szpener <dawidszpener141@gmail.com>
2020-02-13 18:08:46 +0000

    style: text style and adding rules

--- client/src/components/Text/Text.jsx
+++ client/src/components/Text/Text.jsx
@@ -1,22 +1,21 @@
 import React, { Fragment } from 'react'
-import logo from './logo.png'
 import './Text.css'
 
 export const Text = () =>
   <Fragment>
     <div className="description-div">
-      <img src={logo} alt="Logo" width="300" height="200" />
-      <br></br><br></br><br></br>
-    Is a collection of cells arranged in a grid.
-    Each cell changes state according to
-    a defined set of rules:
-      <br></br>
-    1.
-      <br></br>
-    2.
-      <br></br>
-    3.
-      <br></br>
-    4.
+      <p className="title">Cellular Automaton</p>
+      <p className="text">
+        Is a collection of cells arranged in a grid.
+        Each cell changes state according to
+        a defined set of rules:
+        <br></br><br></br>
+        1. Any live cell with two or three neighbors survives.
+        <br></br><br></br>
+        2. Any dead cell with three live neighbors becomes a live cell
+        <br></br><br></br>
+        3. All other live cells die in the next generation. Similarly, all other dead cells stay dead
+        <br></br>
+      </p>
     </div>
   </Fragment>
