6e45c5424d09f4bfbfcd4f8da18facdce4de673a
Dawid Szpener <dawidszpener141@gmail.com>
2020-02-13 16:01:54 +0000

    style: background image

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import Board from '../Board/Board'
 import { Text } from '../Text/Text'
 import './App.css'
+import background from './background2.png'
 
 const App = () => {
   return (
     <div className="app-container">
+      <div className="bg-color">
+        <img src={background} alt="bg" class="bg"></img>
+      </div>
       <Text />
       <Board />
     </div>
