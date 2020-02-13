6594cd3bd0fe6ee08b5bf624cacde82a77c7b426
Harrison Yan <55198681+Hyan18@users.noreply.github.com>
2020-02-13 11:29:32 +0000

    Code quality (#42)
    * fix: codeclimate ignore tests
    
    * refactor: extract limit forms
    
    * fix: tests use Form components
    
    * fix: delete redundant files/content
    
    * fix: pass linter

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,44 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Board from '../Board/Board'
 import { Text } from '../Text/Text'
 import './App.css'
 
-import exampleService from '../../services/exampleService'
-
 const App = () => {
-  const [examples, setExamples] = useState(null)
-
-  useEffect(() => {
-    if (!examples) {
-      getExamples()
-    }
-  })
-
-  const getExamples = async () => {
-    const res = await exampleService.getAll()
-    setExamples(res)
-  }
-
-  const renderExample = example => {
-    return (
-      <li key={example._id} className="list__item example">
-        <h3 className="example__name">{example.name}</h3>
-        <p className="example__description">{example.description}</p>
-      </li>
-    )
-  }
-
   return (
     <div className="app-container">
       <Text />
       <Board />
-      <ul className="example_list">
-        {(examples && examples.length > 0) ? (
-          examples.map(example => renderExample(example))
-        ) : (
-          <p>No examples found</p>
-        )}
-      </ul>
     </div>
   )
 }
